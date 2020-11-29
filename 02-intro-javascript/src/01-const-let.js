bdaafd6fd8909b3723c9e66908b2f8cd6cdc8800
MaurooRen <maurooren@gmail.com>
2020-11-28 21:04:58 -0500

    some practical examples of const, let and scope

---
+++ 02-intro-javascript/src/01-const-let.js
@@ -0,0 +1,18 @@
+// Variables y Constantes
+
+const name = 'Mauricio'
+let lastName = 'Rengifo'
+
+let age = 23
+
+age = 24
+
+console.log(name, lastName, age)
+
+//var NO se va a usar de ahora en mas...
+if (true) {
+    const name = 'Albert'
+    console.log(name)
+}
+
+console.log(name) 
