62f84acc8ddac3015503ca63905f4e7b3c494a0f
MaurooRen <maurooren@gmail.com>
2020-12-06 13:38:21 -0500

    using ReactDOM to render an html tag

--- 03-counter-app/src/index.js
+++ 03-counter-app/src/index.js
@@ -1,1 +1,10 @@
-const saludo = <h1>Hola mundo</h1>;
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+const saludo = <h1><span>Hola</span> mundo</h1>;
+
+
+const divRoot = document.querySelector('#root')
+
+
+ReactDOM.render(saludo, divRoot)
