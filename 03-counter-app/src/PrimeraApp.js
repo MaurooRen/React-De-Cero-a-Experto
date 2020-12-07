9067ed7ffcdba2d2dfd03f63757a1f028dae3bc1
MaurooRen <maurooren@gmail.com>
2020-12-07 09:57:09 -0500

    printing variables on components

--- 03-counter-app/src/PrimeraApp.js
+++ 03-counter-app/src/PrimeraApp.js
@@ -1,22 +1,14 @@
 import React from 'react';
-// import React, { Fragment } from 'react';
 
 // Funcional Components
 
 const PrimeraApp = () => {
-    // Para funcionar SIEMPRE debe estar dentro de un div, de NO QUERER EL DIV, debemos encapsular el codigo en un <Fragment>
-    // return (
-    //     <Fragment>
-    //         <h1><span>Hola</span> mundo</h1>
-    //         <p>Parrafo</p>
-    //     </Fragment>
-    // );
 
-    
-    //Esta es la forma corta de hacer el <Fragment></Fragment> y no requiere importaciones
+    const saludo = 'Hola Mundo Mundial!'
+
     return (
         <>
-            <h1><span>Hola</span> mundo</h1>
+            <h1>{ saludo }</h1>
             <p>Parrafo</p>
         </>
     );
